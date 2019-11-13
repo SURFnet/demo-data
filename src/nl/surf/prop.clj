7c78f96f2440953e2171bb948f0fa92de694f2d5
Joost Diepenmaat <joost@zeekat.nl>
2019-11-13 16:02:13 +0100

    project scaffolding

---
+++ src/nl/surf/prop.clj
@@ -0,0 +1,6 @@
+(ns nl.surf.prop)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/nl/surf/prop_test.clj
@@ -0,0 +1,7 @@
+(ns nl.surf.prop-test
+  (:require [clojure.test :refer :all]
+            [nl.surf.prop :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
