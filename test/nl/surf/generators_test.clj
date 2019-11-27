fc3ff47583d6e41a9781045256123d21ab9a57e8
Joost Diepenmaat <joost@zeekat.nl>
2019-11-27 16:08:01 +0100

    Remove dependency on world from generators test

--- test/nl/surf/generators_test.clj
+++ test/nl/surf/generators_test.clj
@@ -1,20 +1,10 @@
 (ns nl.surf.generators-test
   (:require [nl.surf.generators :as generators]
-            [nl.surf.world :as world]
             [clojure.test :refer [deftest testing is]]))
 
 (deftest test-generators
   (testing "int-generator"
-    (let [world (world/gen #{{:name      :person/id
-                              :generator generators/int}}
-                           {:person 10})]
-      (is (= 10 (count (:person world))))
-      (is (every? int? (map :person/id (:person world))))))
+    (is (every? int? (repeatedly 10 #(generators/int {})))))
   (testing "one-of-generator"
-    (let [world (world/gen #{{:name      :person/id
-                              :generator (generators/one-of #{1 2 3})}}
-                           {:person 10})]
-      (is (= 10 (count (:person world))))
-      (is (every? #{1 2 3} (map :person/id (:person world)))))))
-
-
+    (let [values #{1 2 3}]
+      (is (every? values (repeatedly 10 #((generators/one-of values) {})))))))
