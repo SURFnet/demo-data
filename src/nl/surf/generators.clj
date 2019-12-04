5b84f7f179f347e7e2b29327b7624d9dff7dd4ae
Remco van 't Veer <remco@remworks.net>
2019-12-04 09:47:41 +0100

    Drop unused specs

--- src/nl/surf/generators.clj
+++ src/nl/surf/generators.clj
@@ -1,23 +1,6 @@
 (ns nl.surf.generators
-  (:require [clojure.data.generators :as gen]
-            [clojure.spec.alpha :as s])
+  (:require [clojure.data.generators :as gen])
   (:refer-clojure :exclude [int]))
-
-(s/def ::name qualified-keyword?)
-(s/def ::deps (s/coll-of ::name))
-(s/def ::constraints (s/coll-of fn?))
-
-(s/def ::attr (s/keys :req-un [::name ::generator]
-                      :opt-un [::deps ::constraints]))
-
-(s/def ::world (s/map-of keyword? (s/coll-of ::entity)))
-
-(s/def ::gen-state (s/keys :req-un [::entity ::attr ::world]))
-
-(s/def ::generator (s/fspec :args (s/cat :state ::gen-state)))
-
-(s/def ::_id uuid?)
-(s/def ::entity (s/keys :opt-un [::_id]))
 
 (defn uuid
   []
