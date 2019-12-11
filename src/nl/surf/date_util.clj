84cd93860b34b569cb2346c38adb2d68d46166cc
Remco van 't Veer <remco@remworks.net>
2019-12-11 11:09:39 +0100

    First take on OOAPI
    Institution, educational-programmes, courses, course-offering and
    persons.

---
+++ src/nl/surf/date_util.clj
@@ -0,0 +1,78 @@
+(ns nl.surf.date-util
+  (:refer-clojure :exclude [get])
+  (:import java.text.SimpleDateFormat
+           java.util.Calendar))
+
+(defn simple-format [^String format ^Calendar inst]
+  (.format (doto (SimpleDateFormat. format) (.setCalendar inst)) (.getTime inst)))
+
+(defn rfc3339
+  "Format given `inst` to RFC 3339 defined string (only the date part)."
+  [^Calendar inst]
+  (simple-format "yyyy-MM-dd'T'HH:mm:ssXXX" inst))
+
+(defn rfc3339-date
+  "Format given `inst` to RFC 3339 defined string."
+  [^Calendar inst]
+  (simple-format "yyyy-MM-dd" inst))
+
+(defn parse-date
+  [^String s]
+  (doto (java.util.Calendar/getInstance)
+    (.setTime (.parse (SimpleDateFormat. "yyyy-MM-dd") s))))
+
+(def january Calendar/JANUARY)
+(def february Calendar/FEBRUARY)
+(def march Calendar/MARCH)
+(def april Calendar/APRIL)
+(def may Calendar/MAY)
+(def june Calendar/JUNE)
+(def july Calendar/JULY)
+(def august Calendar/AUGUST)
+(def september Calendar/SEPTEMBER)
+(def october Calendar/OCTOBER)
+(def november Calendar/NOVEMBER)
+(def december Calendar/DECEMBER)
+
+(def sunday Calendar/SUNDAY)
+(def monday Calendar/MONDAY)
+(def tuesday Calendar/TUESDAY)
+(def wednesday Calendar/WEDNESDAY)
+(def thursday Calendar/THURSDAY)
+(def friday Calendar/FRIDAY)
+(def saturday Calendar/SATURDAY)
+
+(defn nth-weekday-of
+  "Return `java.util.Calendar` instance of the `n`-th (starting at `0`)
+  `weekday` of `month` in `year`."
+  [n weekday year month]
+  (loop [cal (doto (Calendar/getInstance) (.setTimeInMillis 0)
+                   (.set year month 1 0 0 0))]
+    (if (= weekday
+           (.get cal Calendar/DAY_OF_WEEK))
+      (doto cal (.add Calendar/DAY_OF_MONTH (* n 7)))
+      (recur (doto cal (.add Calendar/DAY_OF_MONTH 1))))))
+
+(defn last-day-of
+  "Return `java.util.Calendar` instance of the last day of `month` in `year`."
+  [year month]
+  (doto (Calendar/getInstance) (.setTimeInMillis 0)
+        (.set year month 1 0 0 0)
+        (.add Calendar/MONTH 1)
+        (.add Calendar/DAY_OF_MONTH -1)))
+
+(def month Calendar/MONTH)
+(def year Calendar/YEAR)
+
+(defn get
+  "Get property of `java.util.Calendar`."
+  [cal prop]
+  (.get cal prop))
+
+(defn ->msecs-since-epoch
+  [^Calendar cal]
+  (.getTimeInMillis cal))
+
+(defn <-msecs-since-epoch
+  [^long msecs]
+  (doto (Calendar/getInstance) (.setTimeInMillis msecs)))
--- src/nl/surf/generators.clj
+++ src/nl/surf/generators.clj
@@ -4,6 +4,7 @@
             [clojure.data.generators :as gen]
             [clojure.java.io :as io]
             [clojure.string :as s]
+            [nl.surf.markov-chain :as mc]
             [yaml.core :as yaml]))
 
 (defn uuid
@@ -45,6 +46,15 @@
   (fn [_]
     (apply gen/one-of coll)))
 
+(defn weighted
+  "Build a generator to pick one of weighted `m`.
+
+  For example: with `{\"foo\" 2, \"bar\" 1}` there's a 2 in 3 chance `\"foo\"`
+  will be picked."
+  [m]
+  (fn [_]
+    (gen/weighted m)))
+
 (defn format
   "Build a generator for strings using a `fmt` as in `clojure.core/format` and
   `arg-gens` generators as arguments."
@@ -53,14 +63,27 @@
     (let [args (map (fn [gen] (gen world)) arg-gens)]
       (apply core/format fmt args))))
 
+(defn text
+  [corpus & {:keys [lines lookback] :or {lines 3, lookback 2}}]
+  (let [state-space (mc/analyse-text corpus)]
+    (fn [world]
+      (->> (repeatedly #(mc/generate-text state-space))
+           (take lines)
+           (s/join "  ")))))
+
 ;;;; Helpers
+
+(defn resource
+  "Return the full text from the named resource `n`."
+  [n]
+  (-> n io/resource slurp))
 
 (defn lines-resource
   "Returns a collection by reading named resource `n` and spliting lines."
   [n]
-  (-> n io/resource slurp s/split-lines))
+  (-> n io/resource io/reader line-seq))
 
 (defn yaml-resource
   "Returns YAML data from resource `n`."
   [n]
-  (-> n io/resource slurp (yaml/parse-string :keywords false)))
+  (-> n resource (yaml/parse-string :keywords false)))
