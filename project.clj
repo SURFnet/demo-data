dfb1d7e1805a5a1bbe4074f22f2dc4ada21f895d
Jelmer de Ronde <jelmerderonde@gmail.com>
2020-02-19 12:21:21 +0100

    Preparing for first release

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
-(defproject nl.surf/demo-data "0.1.0-SNAPSHOT"
+(defproject nl.surf/demo-data "1.0.0"
   :description "SURFnet DemoData toolkit"
+  :url "https://github.com/SURFnet/demo-data"
   :dependencies [[io.forward/yaml "1.0.9" :exclusions [org.flatland/ordered]]
                  [markov-chain "0.1.0"]
                  [org.clojure/clojure "1.10.1"]
