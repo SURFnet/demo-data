e6f6e65a8b60957eb5f54d43b4950333a7fb01b0
Remco van 't Veer <remco@remworks.net>
2024-01-22 13:24:51 +0100

    Replace io.forward/yaml with clj-commons/clj-yaml

--- project.clj
+++ project.clj
@@ -3,13 +3,12 @@
   :license {:name "GPLv3"
             :url "https://www.gnu.org/licenses/gpl-3.0.en.html"}
   :url "https://github.com/SURFnet/demo-data"
-  :dependencies [[io.forward/yaml "1.0.9" :exclusions [org.flatland/ordered]]
-                 [markov-chain "0.1.0"]
-                 [org.clojure/clojure "1.10.1"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/data.generators "0.1.2"]
                  [org.clojure/math.combinatorics "0.1.6"]
-                 [org.flatland/ordered "1.5.7"] ; compatible with JDK 11
-                 [cheshire/cheshire "5.9.0"]]
+                 [clj-commons/clj-yaml "1.0.27"]
+                 [cheshire/cheshire "5.9.0"]
+                 [markov-chain "0.1.0"]]
   :repl-options {:init-ns nl.surf.demo-data.world}
   :resource-paths ["resources" "generated"]
   :profiles {:dev     {:resource-paths ["dev-resources"]}
